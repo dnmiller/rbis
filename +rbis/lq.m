4aebddfc0bb1ccd95b63ee569ebb5b277f462961
Dan Miller <dnmiller@gmail.com>
2012-09-23 14:29:37 -0700

    Adding LQ function.

---
+++ +rbis/lq.m
@@ -0,0 +1,17 @@
+function [L Q] = lq(A)
+%lq: Triangular-orthogonal decomposition.
+%
+%   [L, Q] = lq(A), where A is m-by-n, produces an m-by-m lower triangular
+%   matrix L and an n-by-n unitary matrix Q so that A = L*Q.
+%
+% References: 
+% [1] Katayama, Subspace Methods for System Identification, 2005.
+
+narginchk(1, 1);
+nargoutchk(0, 2);
+validateattributes(A, {'numeric'}, {'2d'});
+
+[Q, L] = qr(A', 0);
+
+Q = Q';
+L = L';
