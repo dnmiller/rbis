29d6e5eab916aed732cad15acd8740cbb2231fa1
Dan Miller <dnmiller@gmail.com>
2012-09-23 12:02:46 -0700

    Adding license and package header.

---
+++ rbis.m
@@ -0,0 +1,19 @@
+function rbis
+% Realization-Based Identification Software Toolbox
+% -------------------------------------------------------------------------
+% This Matlab toolbox contains functions for the generalized realization
+% algorithm (GRA) and the correlation-based realization algorithm (COBRA),
+% originally developed by the System Identification and Control Laboratory,
+% part of the Cymer Center for Control Systems and Dynamics at the
+% University of California San Diego, Jacobs School of Engineering.
+%
+% The files are contained in the directory "+rbis". To access the
+% functions without modifying your path, use the syntax
+%
+% >> rbis.[function name]
+%
+% To see all available functions, type "rbis." folowed by the tab key.
+%
+% Type rbis.version to see current version. 
+disp('Type "help rbis" for usage.');
+end
