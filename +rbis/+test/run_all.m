d9a349976672edee7b400ad448854905e99b9974
Dan Miller <dnmiller@gmail.com>
2012-10-06 15:56:18 -0700

    Updating test runner.

--- +rbis/+test/run_all.m
+++ +rbis/+test/run_all.m
@@ -3,8 +3,15 @@ function run_all
 try
     rbis.test.test_blkhankel;
     rbis.test.test_datahankel;
+    rbis.test.test_hokalman;
 catch err
-    msg = sprintf('%s\n\n%s', err.message, ...
-    	'Core functions failed. Something is wrong. Please report this error.');
-    throw(MException('rbis:TestAll:CorTests', msg));
+    if strcmp(err.identifier, 'MATLAB:UndefinedFunction')
+        msg = sprintf('%s\n%s', err.message, ...
+            'Missing files. Is mxunit in your path?');
+        throw(MException('rbis:TestAll:UndefinedFunction', msg)); 
+    else
+        msg = sprintf('%s\n\n%s', err.message, ...
+            'Core functions failed. Something is wrong. Please report this issue at http://github.com/dnmiller/rbis');
+        throw(MException('rbis:TestAll:CoreTests', msg));
+    end
 end
