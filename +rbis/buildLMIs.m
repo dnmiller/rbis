48edd46f49fa9414f2910694ad6e37b46ca72d27
Dan Miller <dnmiller@gmail.com>
2012-10-13 16:26:52 -0700

    Finished buildLMIs, updated README.

--- +rbis/buildLMIs.m
+++ +rbis/buildLMIs.m
@@ -1,8 +1,8 @@
-function [P, Q, M] = buildLMIs(n, props, args)
+function [P, Q, M] = buildLMIs(n, region, varargin)
 %buildLMIs: Construct LMIs to describe a convex region of the complex
 %plane.
 % 
-%   [P, Q, M] = buildLMIs(n, props, args)
+%   [P, Q, M] = buildLMIs(n, region, ...)
 % 
 %       Construct YALMIP LMI objects that describe a convex region of the
 %       complex plane. P is a symmetric matrix, Q is a full matrix, and M
@@ -11,7 +11,7 @@ function [P, Q, M] = buildLMIs(n, props, args)
 %       0 and M >= 0 will have a third parameter A = P\Q which has
 %       eigenvalues within the complex plane described by M.
 % 
-%       The props parameter describes the LMI regions. There are three
+%       The region parameter describes the LMI regions. There are three
 %       combinations of LMI regions, and any intersection of the three also
 %       forms an LMI region. The three regions have the following possible
 %       descriptions:
@@ -31,8 +31,9 @@ function [P, Q, M] = buildLMIs(n, props, args)
 %               - dp may be optionally set by the 'DeltaP' property.
 %               - by default, dp = 1e-4.
 % 
-%       props may be either a string or a cell array of strings. args are
-%       the Delta* properties described in Property, Value pairs.
+%       region may be either a string or a cell array of strings. The
+%       remaining arguments are the Delta* properties described in
+%       Property, Value pairs.
 % 
 %       Examples:
 % 
@@ -51,55 +52,81 @@ function [P, Q, M] = buildLMIs(n, props, args)
 % 
 %       solvesdp([P >= 0; M >= 0], J)
 % 
-%       will minimize the cost-function J, which is likely a YALMIP object
-%       described in terms of P and Q.
+%       will minimize the cost-function J, which should be a YALMIP object
+%       described in terms of P and Q. 
 % 
 % Source: Miller, de Callafon, "Subspace Identification with Eigenvalue
 % Constraints," to appear in Automatica sometime in 2012 or 2013.
 
+% Note: There's no unit test for this. We assume the deterministic solution
+% tests are sufficient to verify that they function correctly.
+
 % (C) D. Miller, 2012.
+pkgname = 'rbis';
+errid = @(x) [pkgname, ':buildLMIs:', x];
 
-% TODO: parse props.
-    if isempty(props.DeltaS)
-        ds = 1e-4;
-    else
-        ds = props.DeltaS;
-    end
+assert(exist('sdpvar', 'file') == 2, errid('NoYALMIP'), ...
+    'YALMIP function sdpvar not found on current path. Aborting.');
+narginchk(2, 8);
+
+% Use Matlab's built-in inputParser object to parse the input arguments.
+isorder = @(x) validateattributes(x, ...
+    {'numeric'}, {'real', 'scalar', 'integer', 'positive'});
+isdelta = @(x) validateattributes(x, ...
+    {'numeric'}, {'real', 'scalar', 'finite', 'nonnan', 'positive'});
+
+p = inputParser;
+p.addRequired('n', isorder);
+p.addRequired('Region', @(r) ischar(r) || iscell(r));
+p.addParamValue('DeltaS', 1e-4, isdelta);
+p.addParamValue('DeltaR', 1e-8, isdelta);
+p.addParamValue('DeltaP', 1e-4, isdelta);
+p.parse(n, region, varargin{:});
+
+ds = p.Results.DeltaS;
+dr = p.Results.DeltaR;
+dp = p.Results.DeltaP;
+
+% Force single-region entries to be cell arrays.
+if ischar(region)
+    region = {region};
+end
+region = region(:);
+assert(~isempty(region) && length(region) < 4, errid('BadRegions'), ...
+            'Must supply between 1 and 3 regions.');
+
+% Store region options in a map with char keys and logical values.
+validRegions = {'Stable', 'Real', 'Positive'};
+props = containers.Map(validRegions, [false, false, false]);
+for i = 1:length(region)
+    validatestring(region{i}, validRegions);
+    props(region{i}) = true;
+end
+
+% Build the LMIs.
+P = sdpvar(n);
+Q = sdpvar(n, n, 'full');
+
+% This is our constraint matrix. It is a block-diagonal matrix in which
+% each constraint corresponds to one block.
+M = [];
+
+if props('Stable')
+    M = blkdiag(M, [
+            (1 - ds)*P,     Q;
+            Q',             (1 - ds)*P]);
+end
+
+if props('Real')
+    M = blkdiag(M, [
+            dr*P,           0.5*(Q' - Q)
+            0.5*(Q - Q'),   dr*P]);
+end
+
+if props('Positive')
+    M = blkdiag(M, [
+            2*dp*P,         zeros(n);
+            zeros(n),       Q + Q' - 2*dp*P]);
+end
     
-    if isempty(props.DeltaR)
-        dr = 1e-8;
-    else
-        dr = props.DeltaR;
-    end
-    
-    if isempty(props.DeltaP)
-        dp = 1e-4;
-    else
-        dp = props.DeltaP;
-    end
-    
-    P = sdpvar(n);
-    Q = sdpvar(n, n, 'full');
-
-    % This is our constraint matrix. It is a block-diagonal matrix
-    % in which each constraint corresponds to one block.
-    M = [];
-
-    if any(strcmpi(props.Eigenvalues, 'Stable'));
-        M = blkdiag(M, [
-                (1 - ds)*P,     Q;
-                Q',             (1 - ds)*P]);
-    end
-
-    if any(strcmpi(props.Eigenvalues, 'Real'))
-        M = blkdiag(M, [
-                dr*P,           0.5*(Q' - Q)
-                0.5*(Q - Q'),   dr*P]);
-    end
-
-    if any(strcmpi(props.Eigenvalues, 'Positive'));
-        M = blkdiag(M, [
-                2*dp*P,         zeros(n);
-                zeros(n),       Q + Q' - 2*dp*P]);
-    end
 end
--- README.md
+++ README.md
@@ -5,14 +5,15 @@ Realization-Based Identification Software
 
 Functions
 ---------
-- blkhankel: Construct block-hankel matrix from a matrix row and matrix
+- `blkhankel`: Construct block-hankel matrix from a matrix row and matrix
   column.
-- datahankel: Construct block-hankel matrix of data from either vector- or
+- `datahankel`: Construct block-hankel matrix of data from either vector- or
   matrix-valued signals.
-- hokalman: Generate a state-space realization from Markov parameters via
+- `hokalman`: Generate a state-space realization from Markov parameters via
   the Ho-Kalman algorithm.
-- nullproj: Calculate the projection of one matrix onto the null-space of
+- `nullproj`: Calculate the projection of one matrix onto the null-space of
   another.
+- `buildLMIs`: Generate LMIs for solving eigenvalue-constrained problems.
 
 This software is released under the 3-clause BSD license. See LICENSE.txt
 for details.
